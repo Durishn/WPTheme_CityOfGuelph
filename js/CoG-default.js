fd214554e3392e28b51f47a7bc3b65fe189cc454
Nic Durish <n.durish@gmail.com>
2020-03-31 10:43:00 -0400

    Fixing UberMenu WCAG errors

--- js/CoG-default.js
+++ js/CoG-default.js
@@ -24,3 +24,33 @@ topbtn.on('click', function(e) {
   $root.animate({scrollTop:0}, '500');
   return false;
 });
+
+
+
+/*
+* WCAG QUICKFIXES
+*/
+
+/* A 1.3.1 -Warning
+* i used to format text
+*/
+$('i').each(function() {
+  var outer = this.outerHTML;
+
+  // Replace opening tag
+  var regex = new RegExp('<' + this.tagName, 'i');
+  var newTag = outer.replace(regex, '<' + 'span');
+
+  // Replace closing tag
+  regex = new RegExp('</' + this.tagName, 'i');
+  newTag = newTag.replace(regex, '</' + 'span');
+
+  $(this).replaceWith(newTag);
+});
+
+/* A 1.3.1
+* No top-level heading on the page - Warning
+*/
+if ($("h1").length == 0) {
+  $("<h1 class='visually-hidden' role='complementary' aria-label='hidden title'> "+ $("h2").text() + " </h1>").insertAfter( "#main-header");
+}
